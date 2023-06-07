059402acd6656229967f6055d5ea83545a0e0233
jiayangli1994 <52014604+jiayangli1994@users.noreply.github.com>
2023-06-07 15:27:27 -0500

    1 file modified, 1 added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,10 @@
 <template>
 <div style="display:inline-block;float:left">
-<label>search user by DOB <lablel>
-<input type="text" v-model="searchDOB">
-</div>
-<div style="margin-left:20px;display:inline-block;float:left">
-<label>search user by User Name <lablel>
-<input type="text" v-model="searchUserName">
+<label>search user by DOB or User Name <lablel>
+<input type="text" v-model="search">
 </div>
 
-  <div class="hello" v-for="user in users" :key="user.userName">
+  <div class="hello" v-for="(user,index) in userlist" :key="index">
     <div>User Name : {{ user.userName }}</div>
     <div>DOB : {{ user.DOB }}</div>
     <div>Profession : {{ user.Profession }}</div>
@@ -24,19 +20,21 @@ export default {
   props: {
     msg: String,
   },
-  data() {
-    return { 
-      searchDOB:"",
-      searchUserName:"",
+  data() { 
+    return {
+      search:'',
       users: userData };
   },
   computed: {
-    filteredDOB() {
-      return this.users.filter(user=>user.DOB.toLowerCase().includes(this.searchDOB));
-      
-    },
-    filteredUserName() {
-      return this.users.filter(user=>user.userName.toLowerCase().includes(this.searchUserName));
+    userlist() {
+      for (let i = 0;i<this.users.length;i++) {
+        if (this.users[i].DOB.toLowerCase().includes(this.search.trim().toLowerCase())) {
+          return this.users.filter(user=>user.DOB.toLowerCase().includes(this.search.trim().toLowerCase()))
+        }
+        if (this.users[i].userName.toLowerCase().includes(this.search.trim().toLowerCase())) {
+          return this.users.filter(user=>user.userName.toLowerCase().includes(this.search.trim().toLowerCase()));
+        }
+      }
     }
 
   }
